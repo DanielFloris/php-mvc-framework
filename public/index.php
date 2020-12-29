d19e214e6726a3de0d72b85f91fa8dcffa6f8012
damask <danieljfloris@gmail.com>
2020-12-29 22:08:48 +1100

    Basic Views working

--- public/index.php
+++ public/index.php
@@ -4,12 +4,8 @@ require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
 use app\core\Router;
 
-$app = new Application();
-$app->router->get('/', function(){
-    return 'Hello World';
-});
-$app->router->get('/contact', function(){
-    return 'Contact';
-});
+$app = new Application(dirname(__DIR__));
+$app->router->get('/', 'home');
+$app->router->get('/contact', 'contact');
 
 $app->run();
